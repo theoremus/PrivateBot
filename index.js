654eb0195d977b3e76379fe13cd1cd863ba28b4d
Theo Remus <30575905+theoremus@users.noreply.github.com>
2021-09-25 15:16:11 +0000

    add weather embed via WeatherAPI.com

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//----------------------------- prerequisites --------------------------------
 // keep server alive
 process.on("exit", async () => {
   console.log("exit…");
@@ -27,7 +28,10 @@ const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILDS]
 });
 
-// constants
+// load axios for API usage
+const axios = require("axios");
+
+//---------------------------- constants ------------------------------------
 const commandsEmbed = new Discord.MessageEmbed()
   .setColor("#0091ff")
   .setTitle("Commands Help!")
@@ -39,7 +43,39 @@ const commandsEmbed = new Discord.MessageEmbed()
   )
   .setTimestamp();
 
-// define reactions
+//----------------------------- functions ------------------------------------
+async function getWeather(message, location) {
+  console.log(`WeatherAPI: Request current weather data for ${location} on behalf of ${message.author.username}`)
+  await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_TOKEN}&q=${location}&aqi=no`)
+    .then(data => data.data)
+    .then(body => 
+      {
+        const theembed = new Discord.MessageEmbed()
+	            .setColor('#0099ff')
+	            .setTitle(`Weather in ${body.location.name}, ${body.location.country}`)
+	            .setAuthor('WeatherAPI.com', 'https://cdn.weatherapi.com/v4/images/weatherapi_logo.png', 'https://www.weatherapi.com/')
+	            .setDescription(`**${body.current.condition.text}** with **${body.current.humidity}%** humidity and **${body.current.wind_kph} km/h (${body.current.wind_mph} mph)** ${body.current.wind_dir} winds.`)
+	            .setThumbnail(`https:${body.current.condition.icon}`)
+	            .addFields(
+		            { name: '🌡 Temperature:', value: `${body.current.temp_c} °C/${body.current.temp_f} °F`, inline: true },
+		            { name: '🤷 Feels like:', value: `${body.current.feelslike_c} °C/${body.current.feelslike_f} °F`, inline:true },
+		            { name: '🌧 Precip:', value: `${body.current.precip_mm} mm/${body.current.precip_in} in`, inline: true },
+		            { name: '⛅ Cloudcover:', value: `${body.current.cloud}%`, inline: true },
+                { name: '👁 Visibility:', value: `${body.current.vis_km} km/${body.current.vis_miles} mi`, inline: true },
+                { name: '🗜 Pressure:', value: `${body.current.pressure_mb} mbar/${body.current.pressure_in} in`, inline: true }
+	            )
+	            .setTimestamp()
+	            .setFooter(`Last updated: ${body.current.last_updated}`);
+        message.reply({embeds: [theembed]})
+          .then(console.log)
+          .catch(console.error);
+      }
+    )
+    .then(console.log)
+    .catch(console.error);
+}
+
+//------------------------- define reactions ---------------------------------
 // on command
 client.on("messageCreate", message => {
   console.log(
@@ -80,6 +116,15 @@ client.on("messageCreate", message => {
       ]
     });
   }
+  // get weather
+  if (message.content.startsWith(prefix + "weather")) {
+    var tmp = message.content.split(" ")[1];
+    if (tmp == undefined) {
+      message.reply("Please provide a valid location for your request, i.e. `!weather <location>`");
+    } else {
+      getWeather(message, tmp);
+    }
+  }
 });
 
 // on ready state
@@ -87,5 +132,5 @@ client.on("ready", () => {
   console.log(client.user.username + " is ready!");
 });
 
-// login client
+//---------------------------- login client ---------------------------------
 client.login(process.env.TOKEN);
